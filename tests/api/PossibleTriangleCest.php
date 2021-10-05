85f7add89e6568e34b49058b2f27a7826fd5850b
b.marchenko <Dontapproachmei@gmail.com>
2021-10-05 15:57:13 +0400

    тест2

--- tests/api/PossibleTriangleCest.php
+++ tests/api/PossibleTriangleCest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Api;
 
 use Codeception\Example;
+use Codeception\Util\HttpCode;
 use Step\TriangleTester;
 
 class PossibleTriangleCest
@@ -15,62 +16,402 @@ class PossibleTriangleCest
      *
      * @dataProvider triangleDataProvider
      */
-    public function isPossible(TriangleTester $I, Example $provider): void
+
+
+    public function isPossibleGetTriangleData(TriangleTester $I, Example $provider): void
     {
         $I->isPossible($provider['params']);
-        $I->seeResponseContainsJson($provider['expectedArray']);
+        $I->seeResponseContainsJson($provider['expectedArray']); //надо тринглтестер переместить сюда, ибо тут не понятно что он делает метод
+        $I->seeResponseCodeIs($provider['expectedCode']);
+    }
+    public function isPossibleShouldFailWhenNotGetMethod(TriangleTester $I): void
+    {
+        $I->sendPost(TriangleTester::URL . '/possible', ['a' => 10, 'b' => 9, 'c' => 9]);
+        $I->seeResponseCodeIs(HttpCode::METHOD_NOT_ALLOWED);
     }
 
     protected function triangleDataProvider(): array
     {
         return [
-
-            ['params' => [], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['a' => 1], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['b' => 1], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['c' => 1], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['a' => 1, 'b' => 2], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['a' => 1, 'c' => 1], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['b' => 1, 'c' => 1], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['a' => 'hello', 'b' => 1, 'c' => 10], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['a' => 1, 'b' => 'hello', 'c' => 10], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['a' => 1, 'b' => 2, 'c' => 'hello'], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['a' => -1, 'b' => 2, 'c' => 3], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['a' => 1, 'b' => -2, 'c' => 3], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-            ['params' => ['a' => 1, 'b' => 2, 'c' => -3], 'expectedArray' => ['message' => ['error' => 'Not valid date']]],
-        ];
-    }
-
-    /**
-     * @param TriangleTester $I
-     * @param Example $provider
-     *
-     * @dataProvider naturalNumDataProvider
-     */
-    public function isPossibleNaturalNum(TriangleTester $I, Example $provider): void
-    {
-        $I->isPossible($provider['params']);
-        $I->seeResponseContainsJson($provider['expectedArray']);
-    }
-    protected function naturalNumDataProvider(): array
-    {
-        return [
-            ['params' => ['a' => 0, 'b' => 2, 'c' => 3], 'expectedArray' => ['isPossible' => false]],
-            ['params' => ['a' => 1, 'b' => 0, 'c' => 3], 'expectedArray' => ['isPossible' => false]],
-            ['params' => ['a' => 1, 'b' => 2, 'c' => 0], 'expectedArray' => ['isPossible' => false]],
-            ['params' => ['a' => 1, 'b' => 2, 'c' => 3], 'expectedArray' => ['isPossible' => true]],
-            ['params' => ['a' => 1, 'b' => 3, 'c' => 2], 'expectedArray' => ['isPossible' => true]],
-            ['params' => ['a' => 2, 'b' => 1, 'c' => 3], 'expectedArray' => ['isPossible' => true]],
-            ['params' => ['a' => 2, 'b' => 3, 'c' => 1], 'expectedArray' => ['isPossible' => true]],
-            ['params' => ['a' => 3, 'b' => 1, 'c' => 2], 'expectedArray' => ['isPossible' => true]],
-            ['params' => ['a' => 3, 'b' => 2, 'c' => 1], 'expectedArray' => ['isPossible' => true]],
-            ['params' => ['a' => 1, 'b' => 1, 'c' => 1], 'expectedArray' => ['isPossible' => true]],
-            ['params' => ['a' => 10, 'b' => 2, 'c' => 1], 'expectedArray' => ['isPossible' => false]],
-            ['params' => ['a' => 2, 'b' => 10, 'c' => 1], 'expectedArray' => ['isPossible' => false]],
-            ['params' => ['a' => 2, 'b' => 1, 'c' => 10], 'expectedArray' => ['isPossible' => false]],
-            ['params' => ['a' => 10, 'b' => 9, 'c' => 9], 'expectedArray' => ['isPossible' => true]],
-            ['params' => ['a' => 9, 'b' => 10, 'c' => 9], 'expectedArray' => ['isPossible' => true]],
-            ['params' => ['a' => 9, 'b' => 9, 'c' => 10], 'expectedArray' => ['isPossible' => true]],
+            [
+                'params' => [
+                    'a' => 0,
+                    'b' => 2,
+                    'c' => 3
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' =>
+                    [
+                        'a' => 1,
+                        'b' => 0,
+                        'c' => 3
+                    ],
+                'expectedMessage' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' =>
+                    [
+                        'a' => 1,
+                        'b' => 2,
+                        'c' => 0
+                    ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' =>
+                    [
+                        'a' => 1,
+                        'b' => 2,
+                        'c' => 3
+                    ],
+                'expectedArray' => [
+                    'isPossible' => false
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 1,
+                    'b' => 3,
+                    'c' => 2
+                ],
+                'expectedArray' => [
+                    'isPossible' => false
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 2,
+                    'b' => 1,
+                    'c' => 3
+                ],
+                'expectedArray' => [
+                    'isPossible' => false
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 2,
+                    'b' => 3,
+                    'c' => 1
+                ],
+                'expectedArray' => [
+                    'isPossible' => false
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 3,
+                    'b' => 1,
+                    'c' => 2
+                ],
+                'expectedArray' => [
+                    'isPossible' => false
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 3,
+                    'b' => 2,
+                    'c' => 1
+                ],
+                'expectedArray' => [
+                    'isPossible' => false
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 1,
+                    'b' => 1,
+                    'c' => 1
+                ],
+                'expectedArray' => [
+                    'isPossible' => true
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 10,
+                    'b' => 2,
+                    'c' => 1
+                ],
+                'expectedArray' => [
+                    'isPossible' => false
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 2,
+                    'b' => 10,
+                    'c' => 1
+                ],
+                'expectedArray' => [
+                    'isPossible' => false
+                ],'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 2,
+                    'b' => 1,
+                    'c' => 10
+                ],
+                'expectedArray' => [
+                    'isPossible' => false
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 10,
+                    'b' => 9,
+                    'c' => 9
+                ],
+                'expectedArray' => [
+                    'isPossible' => true
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 9,
+                    'b' => 10,
+                    'c' => 9
+                ],
+                'expectedArray' => [
+                    'isPossible' => true
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [
+                    'a' => 9,
+                    'b' => 9,
+                    'c' => 10
+                ],
+                'expectedArray' => [
+                    'isPossible' => true
+                ],
+                'expectedCode' => HttpCode::OK
+            ],
+            [
+                'params' => [],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 1
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'b' => 1
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'c' => 1
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 1,
+                    'b' => 2
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 1,
+                    'c' => 1
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'b' => 1,
+                    'c' => 1
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 'hello',
+                    'b' => 1,
+                    'c' => 10
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 1,
+                    'b' => 'hello',
+                    'c' => 10
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 1,
+                    'b' => 2,
+                    'c' => 'hello'
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => -1,
+                    'b' => 2,
+                    'c' => 3
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 1,
+                    'b' => -2,
+                    'c' => 3
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 1,
+                    'b' => 2,
+                    'c' => -3
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 1.1,
+                    'b' => 2,
+                    'c' => 3
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 1,
+                    'b' => 2.2,
+                    'c' => 3
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
+            [
+                'params' => [
+                    'a' => 1,
+                    'b' => 2,
+                    'c' => 3.3
+                ],
+                'expectedArray' => [
+                    'message' => [
+                        'error' => 'Not valid data'
+                    ]
+                ],
+                'expectedCode' => HttpCode::BAD_REQUEST
+            ],
         ];
     }
 }
